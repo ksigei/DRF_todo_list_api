905abadeadad3026dfaaf9fadbc64d3d9c75dd47
ksigei <sigeikiprono4@gmail.com>
2023-04-29 10:36:58 +0300

    Create the endpoints

---
+++ todos/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ListTodo.as_view()),
+    path('<int:pk>/', views.DetailTodo.as_view()),
+]
---
+++ todos/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+# Create your views here.
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
